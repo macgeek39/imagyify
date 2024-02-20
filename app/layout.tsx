619b5f544d2e59bd79b7847e3e3c335b71af80f5
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-02-20 04:58:10 -0600

    Created auth folder and layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
 import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({ 
